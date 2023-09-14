750a3db35f607097e143b0d7162c7d7b4c4f921e
geoas <geoas@TF-008459591733>
2023-09-14 00:10:40 -0600

    Primer commit del proyecto. Geo

---
+++ src/main/java/com/Tienda_SC403/TiendaSc403Application.java
@@ -0,0 +1,13 @@
+package com.Tienda_SC403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSc403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSc403Application.class, args);
+	}
+
+}
